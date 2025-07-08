7cae4abb5ad65326e69a3412ac3e024c0992f462
Dmitry029 <kdp.029029@gmail.com>
2025-07-08 15:48:37 +0300

    Доработаны все методы transaction(), xaTransaction() для передачи в них уровня изоляции транзакции.

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -7,36 +7,58 @@ import guru.qa.niffler.data.entity.spend.CategoryEntity;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
+import lombok.AllArgsConstructor;
 
 import static guru.qa.niffler.data.Databases.transaction;
 
 public class SpendDbClient {
 
-  private static final Config CFG = Config.getInstance();
+    private static final Config CFG = Config.getInstance();
 
-  public SpendJson createSpend(SpendJson spend) {
-    return transaction(connection -> {
-          SpendEntity spendEntity = SpendEntity.fromJson(spend);
-          if (spendEntity.getCategory().getId() == null) {
-            CategoryEntity categoryEntity = new CategoryDaoJdbc(connection)
-                .create(spendEntity.getCategory());
-            spendEntity.setCategory(categoryEntity);
-          }
-          return SpendJson.fromEntity(
-              new SpendDaoJdbc(connection).create(spendEntity)
-          );
-        },
-        CFG.spendJdbcUrl()
-    );
-  }
+    @AllArgsConstructor
+    private enum IsolationLevel {
+        READ_UNCOMMITTED(1),
+        READ_COMMITTED(2),
+        REPEATABLE_READ(3),
+        SERIALIZABLE(4);
 
-  public CategoryJson createCategory(CategoryJson categoryJson){
-    CategoryEntity categoryEntity = CategoryEntity.fromJson(categoryJson);
-    return CategoryJson.fromEntity(categoryDao.create(categoryEntity));
-  }
+        private final int value;
+    }
 
-  public void deleteCategory(CategoryJson categoryJson){
-    CategoryEntity categoryEntity = CategoryEntity.fromJson(categoryJson);
-    categoryDao.deleteCategory(categoryEntity);
-  }
+    public SpendJson createSpend(SpendJson spend) {
+
+        return transaction(connection -> {
+                    SpendEntity spendEntity = SpendEntity.fromJson(spend);
+                    if (spendEntity.getCategory().getId() == null) {
+                        CategoryEntity categoryEntity = new CategoryDaoJdbc(connection)
+                                .create(spendEntity.getCategory());
+                        spendEntity.setCategory(categoryEntity);
+                    }
+                    return SpendJson.fromEntity(
+                            new SpendDaoJdbc(connection).create(spendEntity)
+                    );
+                },
+                CFG.spendJdbcUrl(), IsolationLevel.READ_COMMITTED.value
+        );
+    }
+
+    public CategoryJson createCategory(CategoryJson categoryJson) {
+        return transaction(
+                connection -> {
+                    CategoryEntity categoryEntity = CategoryEntity.fromJson(categoryJson);
+                    return CategoryJson.fromEntity(new CategoryDaoJdbc(connection).create(categoryEntity));
+                },
+                CFG.spendJdbcUrl(), IsolationLevel.READ_COMMITTED.value
+        );
+    }
+
+    public void deleteCategory(CategoryJson categoryJson) {
+        transaction(connection -> {
+                    CategoryEntity categoryEntity = CategoryEntity.fromJson(categoryJson);
+                    new CategoryDaoJdbc(connection).deleteCategory(categoryEntity);
+                    return null;
+                },
+                CFG.spendJdbcUrl(), IsolationLevel.READ_COMMITTED.value
+        );
+    }
 }
